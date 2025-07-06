07bf73d7805183fcc590a09478e6e046d0ea34b2
ducduy45 <duckduy45@gmail.com>
2025-07-06 17:38:13 +0700

    updated My Order page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -65,7 +65,7 @@ const Cart = () => {
         <div className='w-full sm:w-[450px]'>
           <CartTotal />
           <div className='w-full text-end'>
-            <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PLACE ORDER</button>
+            <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO ORDER</button>
           </div>
         </div>
       </div>
